a494a09c1bc447b28aa57c34619785d01a12429e
gangstap <30912414+gangstap@users.noreply.github.com>
2026-07-14 00:16:51 +0300

    Update run_universal_config.sh

--- run_universal_config.sh
+++ run_universal_config.sh
@@ -4,12 +4,14 @@ DESCRIPTION=$(ubus call system board | jsonfilter -e '@.release.description')
 VERSION=$(ubus call system board | jsonfilter -e '@.release.version')
 findVersion="24.10.2"
 
+SCRIPT_URL="https://raw.githubusercontent.com/gangstap/283r2jhr3h28o/refs/heads/main"
+
 if printf '%s\n%s\n' "$findVersion" "$VERSION" | sort -V | tail -n1 | grep -qx -- "$VERSION"; then
  printf "\033[32;1mThis new firmware. Running new script...\033[0m\n"
- wget --no-check-certificate -O /tmp/universal_config_new_podkop.sh https://raw.githubusercontent.com/gangstap/283r2jhr3h28o/refs/heads/main/universal_config_new_podkop.sh && chmod +x /tmp/universal_config_new_podkop.sh && /tmp/universal_config_new_podkop.sh $1 $2
+ wget --no-check-certificate -O /tmp/universal_config_new_podkop.sh "$SCRIPT_URL/universal_config_new_podkop.sh" && chmod +x /tmp/universal_config_new_podkop.sh && /tmp/universal_config_new_podkop.sh $1 $2
 else
  printf "\033[32;1mThis old firmware.\nRecommendation: upgrade firmware to actual release...\nSleep 5 sec...\033[0m\n"
  sleep 5
  printf "\033[32;1mRunning old script...\033[0m\n"
- wget --no-check-certificate -O /tmp/universal_config.sh https://raw.githubusercontent.com/gangstap/283r2jhr3h28o/refs/heads/main/universal_config.sh && chmod +x /tmp/universal_config.sh && /tmp/universal_config.sh $1 $2
+ wget --no-check-certificate -O /tmp/universal_config.sh "$SCRIPT_URL/universal_config.sh" && chmod +x /tmp/universal_config.sh && /tmp/universal_config.sh $1 $2
 fi
